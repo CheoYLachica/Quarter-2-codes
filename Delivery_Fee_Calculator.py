5c8345f5115994972215e6bbbd9b213b451267b8
CheoYLachica <lotharcheo.lachica@smc.pshs.edu.ph>
2025-10-13 14:27:47 +0800

    Delivery Fee Caluclator
    This code calculates the delivery fee based on the given rate and distance provided by the user.

---
+++ Delivery_Fee_Calculator.py
@@ -0,0 +1,4 @@
+dkm = float(input("Enter the distance in kilometers: "))
+Rpkm = float(input("Enter the rate per kilometer (₱): "))
+delivery_fee = dkm * Rpkm
+print("The delivery fee is: ₱", delivery_fee)
